cddfdbefabf8e4b2ddfd00bb24a3057c909edc11
neo <paul.gardien@epitech.eu>
2023-02-08 17:55:27 +0100

    Update GUI and logic

--- Drain/Drain/ViewModels/MainViewModel.cs
+++ Drain/Drain/ViewModels/MainViewModel.cs
@@ -2,6 +2,7 @@
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,34 +13,77 @@ namespace Drain.ViewModels
     public class MainViewModel: BaseViewModel
     {
         public SettingsViewModel Settings { get; set; }
-        public EngineViewModel Engine { get; set; }
-
+        public LoggerViewModel Logger { get; set; }
+        private OpenFileDialog openFileDialog { get; set; }
         private DelegateCommand Loader { get; set; }
         public DelegateCommand SelectCommand { get; set; }
+        public DelegateCommand DrainCommand { get; set; }
+        public ManagerViewModel ManagerViewModel { get; set; }
+        private string Root { get; set; }
 
         public MainViewModel()
         {
             Settings = new SettingsViewModel();
-            Engine = new EngineViewModel();
+            Logger = new LoggerViewModel();
             Loader = new DelegateCommand(Load);
+            Root = "Drainned";
+            ManagerViewModel = new ManagerViewModel();
             SelectCommand = new DelegateCommand(Select);
+            DrainCommand = new DelegateCommand(Drain);
 
             Loader.Execute(null);
         }
 
         private void Load(object data)
         {
+            Logger.Record("loading Drain");
+
             Settings.LoadVersion();
+            openFileDialog = new OpenFileDialog();
+            if (Directory.Exists(Root) == false)
+            {
+                Directory.CreateDirectory(Root);
+            }
+
+            Logger.Record("Drain loaded");
         }
 
         private void Select(object data)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-
             if (openFileDialog.ShowDialog() == true)
             {
-                return;
+                Logger.Record("content updated");
+            } else
+            {
+                Logger.Record("content not updated");
             }
+        }
+
+        private void Drain(object data)
+        {
+            string[] lines = LoadContent();
+            string name = null;
+
+            foreach (string line in lines)
+            {
+                name = GetName(line);
+                Logger.Record($"downloading {name}");
+                ManagerViewModel.Download(line, $"{Root}/{name}");
+                Logger.Record("downloaded");
+            }
+            Logger.Record("all content drained");
+        }
+
+        private string GetName(string url)
+        {
+            string[] content = url.Split('/');
+
+            return (content[content.Length - 1]);
+        }
+
+        private string[] LoadContent()
+        {
+            return (File.ReadAllLines(openFileDialog.FileName));
         }
     }
 }
---
+++ Drain/Drain/ViewModels/ManagerViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drain.ViewModels
+{
+    public class ManagerViewModel: BaseViewModel
+    {
+        private WebClient _client;
+        private WebClient Client
+        {
+            get { return (_client); }
+            set { SetProperty(ref _client, value); }
+        }
+
+        public ManagerViewModel()
+        {
+            Client = new WebClient();
+        }
+
+        public void Download(string url, string path)
+        {
+            if (File.Exists(path) == true)
+            {
+                path = $"{path}_{path.GetHashCode()}";
+            }
+
+            Client.DownloadFile(url, path);
+        }
+    }
+}
