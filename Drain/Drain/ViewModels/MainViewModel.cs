e7939d3274a7580d7015738096de8cf704dac1c8
neo <paul.gardien@epitech.eu>
2023-02-08 16:45:01 +0100

    Init
    Init

---
+++ Drain/Drain/ViewModels/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Drain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Drain.ViewModels
+{
+    public class MainViewModel: BaseViewModel
+    {
+        public SettingsViewModel Settings { get; set; }
+        public EngineViewModel Engine { get; set; }
+
+        private DelegateCommand Loader { get; set; }
+        public DelegateCommand GenerateCommand { get; set; }
+
+        public MainViewModel()
+        {
+            Settings = new SettingsViewModel();
+            Engine = new EngineViewModel();
+            Loader = new DelegateCommand(Load);
+            GenerateCommand = new DelegateCommand(Engine.Generate);
+
+            Loader.Execute(null);
+        }
+
+        private void Load(object data)
+        {
+            Settings.LoadVersion();
+        }
+    }
+}
